5e8446843c93677c844df1f1a68f099c839ef362
tefirowo <tacodrilosupremo0@gmail.com>
2026-08-21 16:08:24 -0400

    Iniciando Arch

---
+++ Proyectos/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+ int edad;
+
+ printf("Hola! Por favor, ingresa tu edad: ");
+ scanf("%d", &edad);
+ printf("Tu año de nacimiento es: %d\n", 2026 - edad);
+
+ return 0;
+}
